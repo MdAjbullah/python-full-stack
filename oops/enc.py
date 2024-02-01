1115c25b80839e5e826ea20f42cd8a0bceccfa99
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-02-02 02:02:30 +0530

    py

---
+++ oops/enc.py
@@ -0,0 +1,44 @@
+# solid peincipal
+# s- single responcibility principle
+# o- open closed principle
+# l- liskov substitution principle
+# i- interface segregation principle
+# d- dependency inversion principle
+
+
+# def abc:
+
+
+
+
+# any fool  can write code that a computer can understand
+# good programmers write code that humans can understand
+
+
+
+# dry principal
+# donot repeat yourself
+# kiss
+# keep it simple stupid
+
+
+# acceser and mutator
+# getter and setter
+
+
+
+
+
+class person:
+    def __init__(self):
+       self.age=10
+def get_age(self):
+  return self.age
+def set_age(self,age):
+    self.age=age
+
+
+p1=person()
+
+
+print(p1.get_age())
