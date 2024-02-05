e4fe0241c5e0b384831c6f1a9cfca4c3d55ba1e9
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-02-06 01:09:37 +0530

    range

---
+++ oops/loop-range.py
@@ -0,0 +1,9 @@
+# for loop
+
+# write a progrma to count the number of a in the given string
+s=input("Enter the string: ")
+count=0
+for i in s:
+    if i=='a':
+        count+=1
+print(count)
---
+++ oops/range.py
@@ -0,0 +1,24 @@
+# write a program print n natural number 
+# n=int(input("Enter the number: "))
+# n=range(0,n+1)
+
+# for e in n:
+  
+#     print(e)
+# write a program to print the squre of n natural number
+# n=int(input("Enter the number: "))
+# n=range(0,n+1)
+# for e in n:
+#     print(e**2)
+    
+    
+# In this example, we
+
+# WRITE THE PROGRAM TO PRINT THE N EVEN NUMBER IN REVERSE
+
+n=int(input("Enter the number: "))
+n=range(n*2,0,-2)
+for e in n:
+    if e%2==0:
+        print((e))
+       
